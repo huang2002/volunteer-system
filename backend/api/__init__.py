c3d2761c69cf8e418b33977405fbb44484479b91
hhh <1340545944@qq.com>
2023-02-11 18:20:16 +0800

    fix missing record blueprint

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,4 +1,5 @@
 from .table import table_blueprint
+from .record import record_blueprint
 from .backup import backup_blueprint
 from .import_ import import_blueprint
 from .common import Blueprint
@@ -6,5 +7,6 @@ from .common import Blueprint
 api_blueprint = Blueprint('api', __name__, url_prefix='/api')
 
 api_blueprint.register_blueprint(table_blueprint)
+api_blueprint.register_blueprint(record_blueprint)
 api_blueprint.register_blueprint(backup_blueprint)
 api_blueprint.register_blueprint(import_blueprint)
