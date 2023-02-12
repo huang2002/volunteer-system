c6a610f597601ddcfd417eabd8a3f6009775bd84
hhh <1340545944@qq.com>
2023-02-12 14:20:08 +0800

    improve column names

--- backend/api/import_/common.py
+++ backend/api/import_/common.py
@@ -19,10 +19,10 @@ class ImportTableError(Exception):
         super().__init__(message)
 
 
-# TODO: add more aliases
 # whitespace characters will be ignored automatically
 COLUMN_MAP: dict[str, str] = {
     **dict((col, col) for col in COLUMNS),
+    '学院': 'student_school',
     '学院（全称）': 'student_school',
     '班级': 'student_class',
     '姓名': 'student_name',
@@ -30,22 +30,35 @@ COLUMN_MAP: dict[str, str] = {
     '联系方式': 'student_contact',
     '联系方式（电话）': 'student_contact',
     '联系电话': 'student_contact',
+    '电话': 'student_contact',
+    '志愿时': 'activity_length',
+    '志愿时长': 'activity_length',
     '志愿时长（小时）': 'activity_length',
     '服务开始日期': 'activity_begin',
+    '开始日期': 'activity_begin',
     '服务结束日期': 'activity_end',
+    '结束日期': 'activity_end',
     '服务截止日期': 'activity_end',
+    '截止日期': 'activity_end',
     '服务日期': 'activity_date',
     '服务日期（xxxx/xx/xx）': 'activity_date',
+    '活动名称': 'activity_name',
+    '志愿项目名称': 'activity_name',
     '志愿项目名称（全称）': 'activity_name',
-    '志愿项目类别': 'activity_type',
+    '活动类别': 'activity_type',
     '志愿类别': 'activity_type',
+    '志愿活动类别': 'activity_type',
+    '志愿项目类别': 'activity_type',
     '志愿服务类别': 'activity_type',
     '举办单位': 'activity_host',
+    '负责人姓名': 'manager_name',
     '项目负责人姓名': 'manager_name',
+    '负责人电话': 'manager_contact',
     '项目负责人电话': 'manager_contact',
+    '负责人QQ号': 'manager_qq',
     '项目负责人QQ号': 'manager_qq',
     '备注': 'notes',
 }
 
 # whitespaces will be striped before test
-IGNORED_COLUMN_PATTERN = re.compile(r'^(?:序号|Unnamed:\d+)$', re.I)
+IGNORED_COLUMN_PATTERN = re.compile(r'^(?:记录编号|编号|序号|Unnamed:\d+)$', re.I)
